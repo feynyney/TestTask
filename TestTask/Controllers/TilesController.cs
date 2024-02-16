ad329fba5b37416b180e8d807924e4411205a53a
DmytroMakohonchuk <Dmytro.Makohonchuk@fivesysdev.com>
2024-02-16 17:24:57 +0200

    Added validation to TilesController

--- TestTask/Controllers/TilesController.cs
+++ TestTask/Controllers/TilesController.cs
@@ -19,13 +19,57 @@ namespace TestTask.Controllers
         }
 
         [HttpPost]
-        public ActionResult<TilesResponseModel> test(TilesRequestModel tilesRequest)
+        public ActionResult<TilesResponseModel> OnPageUpdate(TilesRequestModel tilesRequest)
         {
             //написати валідацію (повертати статус коди)
             //Вкладені лісти мають бути однакового розміру
             //Якщо не 1 або 0 => bad request
             //Якщо поле менше 3х3 => bad request
             //
+
+            //validate matrix size
+            if (tilesRequest.Tiles.Count < 2 || tilesRequest.Tiles[0].Count < 2) 
+            {
+                return BadRequest("Matrix cannot be less then 2x2!");
+            }
+
+            //validate whether matrix has wrong values
+            //foreach (var tile in tilesRequest.Tiles)  
+            //{
+            //    foreach(var item in tile)
+            //    {
+            //        if (item != 0 && item != 1)
+            //        {
+            //            return BadRequest("Matrix only applies 0 or 1 as a value!");
+            //        }
+            //    }
+            //}
+
+            for (int i = 0; i < tilesRequest.Tiles.Count; i++)
+            {
+                for (int j = 0; j < tilesRequest.Tiles[i].Count; j++)
+                {
+                    if (tilesRequest.Tiles[i][j] != 0 && tilesRequest.Tiles[i][j] != 1)
+                    {
+                        return BadRequest("Matrix only applies 0 or 1 as a value!");
+                    }
+                }
+
+                if (tilesRequest.Tiles[i].Count != tilesRequest.Tiles[0].Count)
+                {
+                    return BadRequest("Matrix rows must be the same size!");
+                }
+            }
+
+
+            //for (int i = 0; i < tilesRequest.Tiles.Count; i++)
+            //{
+            //    if (tilesRequest.Tiles[i].Count != tilesRequest.Tiles[0].Count)
+            //    {
+            //        return BadRequest("Matrix rows must be the same size!");
+            //    }
+            //}
+            
 
             _tileService.SaveTiles(tilesRequest.Tiles);
 
